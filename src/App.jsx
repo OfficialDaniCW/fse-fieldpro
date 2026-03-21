02e877fc114e0922ba9bbeb43dce9e559c9c1c61
Daniel Calvo-Westcott <danicalvo_1@hotmail.com>
2026-03-21 19:23:52 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import Favorites from './pages/Favorites';
+import Layout from './layout';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
