da90b4e25d8bb21babb5ae418195523db2079bb9
Daniel Calvo-Westcott <danicalvo_1@hotmail.com>
2026-03-21 19:24:25 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Favorites from './pages/Favorites';
-import Layout from './layout';
+import AppLayout from './layout';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
