1fdddb321bba4df66b90bcc28eb78f08e0f81b5e
Daniel Calvo-Westcott <danicalvo_1@hotmail.com>
2026-03-21 18:58:31 +0000

    File changes

--- src/pages/Parts.jsx
+++ src/pages/Parts.jsx
@@ -16,7 +16,7 @@ export default function PartsPage() {
 
   const { data: parts = [], isLoading } = useQuery({
     queryKey: ["parts"],
-    queryFn: () => base44.entities.Part.list("-created_date"),
+    queryFn: () => base44.entities.Part.list("-created_date", 2000),
     initialData: [],
   });
 
