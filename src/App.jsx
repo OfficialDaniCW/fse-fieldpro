6208d50fea477e7ac23a20fba85bb3d2bb87cc8d
Daniel Calvo-Westcott <danicalvo_1@hotmail.com>
2026-03-22 10:11:15 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Favorites from './pages/Favorites';
 import SyncManager from './pages/SyncManager';
 import ImportParts from './pages/ImportParts';
 import BulkUpload from './pages/BulkUpload';
+import OfflineMode from './pages/OfflineMode';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -80,6 +81,11 @@ const AuthenticatedApp = () => {
       <Route path="/BulkUpload" element={
         <LayoutWrapper currentPageName="BulkUpload">
           <BulkUpload />
+        </LayoutWrapper>
+      } />
+      <Route path="/OfflineMode" element={
+        <LayoutWrapper currentPageName="OfflineMode">
+          <OfflineMode />
         </LayoutWrapper>
       } />
       <Route path="*" element={<PageNotFound />} />
